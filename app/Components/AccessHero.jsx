19d43ff7f9f7b8b0edba312eea3820a95cd376da
TheHolyRoller <79761726+TheHolyRoller@users.noreply.github.com>
2026-03-31 07:16:48 +0300

    ts

---
+++ app/Components/AccessHero.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import h from '../Styles/AccessHero.module.css'; 
+import laptop from '../../public/images/laptop.webp'; 
+
+import Image from 'next/image';
+import HeroBackground from '../../public/images/HeroBackground.png'; 
+import lamp from '../../public/images/lamp.png'; 
+
+
+
+function AccessHero() {
+
+
+    return (
+
+    <main className={h.hero}>
+    <Image className={h.heroBackgroundImage} src={lamp} alt="background"    /> 
+
+    <section className={h.heroContentContainer} >
+
+    <div className={h.heroTextContainer}>
+
+        
+
+        <h1 className={h.heroMainHeader}>
+
+        {/* Disabled Students' Allowance  */}
+        ivvi Customer Support
+
+      
+
+        </h1>
+
+        <article  className={h.heroSupportingText}>
+
+        We love to help.
+
+        <p className={h.text}>
+
+        Please Contact us below.
+
+        </p>
+
+
+        </article>
+
+
+        {/* <button className={h.heroCTAButton}>
+          <Link className={h.heroCTALink}  href='/ivvi' alt='ivvi'>
+
+            Try ivvi Notes now 
+
+          </Link>
+
+        </button> */}
+
+    </div>
+
+    </section>
+    </main>
+)
+}
+
+export default AccessHero
---
+++ app/accesstowork/page.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ac from '../Styles/AccessToWork.module.css'; 
+import AccessHero from '../Components/AccessHero';
+
+
+function page() {
+  return (
+    <section className={ac.mainContainer}>
+        
+        <AccessHero/> 
+
+
+    </section>
+  )
+}
+
+export default page
